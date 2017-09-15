9a57d26f93f20c09faea23e8ce4d7113d9c43d01
ubuntu <dnguyen0304@gmail.com>
2017-09-15 16:45:38 -0400

    ME-721: Migrated buildtime Dockerfile.

--- .tmp/build.sh
+++ .tmp/build.sh
@@ -1,43 +1,8 @@
-#!/usr/bin/env bash
-
-set -eu
-
 if [ "${1:-}" = test ]; then
     FOR_TESTING="true"
 else
     FOR_TESTING="false"
 fi
-
-DOMAIN="dnguyen0304"
-NAMESPACE="roomlistwatcher"
-VERSION=$(grep -Po "version='\K\d\.\d\.\d" setup.py)
-REMOTE_SHARED_VOLUME="/tmp/build"
-
-# Clean up existing packages created by previous builds.
-rm --force ${NAMESPACE}*.zip
-
-# Create the buildtime container.
-BUILDTIME_BASE_IMAGE_VERSION="0.1.0"
-tag=${DOMAIN}/${NAMESPACE}-buildtime:${BUILDTIME_BASE_IMAGE_VERSION}
-
-if [ ! -z $(sudo docker images --quiet ${tag}) ]; then
-    docker rmi --force ${tag}
-fi
-docker build \
-    --file docker/buildtime/Dockerfile \
-    --tag ${tag} \
-    --build-arg DOMAIN=${DOMAIN} \
-    --build-arg NAMESPACE=${NAMESPACE} \
-    --build-arg BASE_IMAGE_VERSION=${BUILDTIME_BASE_IMAGE_VERSION} \
-    --build-arg COMPONENT=${NAMESPACE} \
-    .
-
-# Create the package.
-docker run \
-    --rm \
-    --volume $(pwd):${REMOTE_SHARED_VOLUME} \
-    ${tag} \
-    ${NAMESPACE} ${REMOTE_SHARED_VOLUME} ${VERSION}
 
 # Create the container.
 tag=${DOMAIN}/${NAMESPACE}-runtime:${VERSION}
---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -eu
+
+DOMAIN="dnguyen0304"
+NAMESPACE="downloadbot"
+VERSION=$(grep -Po "version='\K\d\.\d\.\d" setup.py)
+REMOTE_SHARED_VOLUME="/tmp/build"
+
+# Clean up existing packages created by previous builds.
+rm --force ${NAMESPACE}*.zip
+
+# Create the buildtime container.
+BUILDTIME_BASE_IMAGE_VERSION="0.1.0"
+tag=${DOMAIN}/${NAMESPACE}-buildtime:${BUILDTIME_BASE_IMAGE_VERSION}
+
+if [ ! -z $(sudo docker images --quiet ${tag}) ]; then
+    docker rmi --force ${tag}
+fi
+docker build \
+    --file docker/buildtime/Dockerfile \
+    --tag ${tag} \
+    --build-arg DOMAIN=${DOMAIN} \
+    --build-arg NAMESPACE=${NAMESPACE} \
+    --build-arg BASE_IMAGE_VERSION=${BUILDTIME_BASE_IMAGE_VERSION} \
+    --build-arg COMPONENT=${NAMESPACE} \
+    .
+
+# Create the package.
+docker run \
+    --rm \
+    --volume $(pwd):${REMOTE_SHARED_VOLUME} \
+    ${tag} \
+    ${NAMESPACE} ${REMOTE_SHARED_VOLUME} ${VERSION}
