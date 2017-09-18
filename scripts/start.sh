f22e15bad0dbd6c6aa800b3a7fd443abbf2c063e
ubuntu <dnguyen0304@gmail.com>
2017-09-18 18:53:59 -0400

    ME-721: Added download directory to runtime container.

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,4 +9,5 @@ docker run \
     --rm \
     --volume $(pwd):/etc/opt/${NAMESPACE} \
     --volume $(pwd):/var/opt/${NAMESPACE}/log \
+    --volume $(pwd):/var/opt/${NAMESPACE}/data \
     dnguyen0304/${NAMESPACE}-runtime:${TAG}
