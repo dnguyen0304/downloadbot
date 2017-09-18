3619a9f22d0bf454fe2908b68b44c0f383bdd2e9
ubuntu <dnguyen0304@gmail.com>
2017-09-18 15:37:15 -0400

    ME-721: Changed runtime base to include configuration.

--- scripts/build-runtime-base.sh
+++ scripts/build-runtime-base.sh
@@ -8,4 +8,5 @@ docker build \
     --file docker/runtime/base/Dockerfile \
     --tag dnguyen0304/downloadbot-runtime-base:${TAG} \
     --build-arg NAMESPACE="downloadbot" \
+    --build-arg CONFIGURATION_FILE_NAME="application.config" \
     .
--- scripts/start.sh
+++ scripts/start.sh
@@ -6,5 +6,6 @@ TAG=$(./scripts/get-package-version.sh)
 
 docker run \
     --rm \
+    --volume $(pwd):/etc/opt/downloadbot \
     --volume $(pwd):/var/opt/roomlistwatcher/log \
     dnguyen0304/downloadbot-runtime:${TAG}
