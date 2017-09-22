9987a2a2abf0361dc1584bbaadca5103eb5edf98
ubuntu <dnguyen0304@gmail.com>
2017-09-22 15:17:34 -0400

    ME-721: Fixed start script not mapping the configuration volume.

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,7 @@ TAG=$(./scripts/get-package-version.sh)
 
 docker run \
     --rm \
-    --volume $(pwd):/etc/opt/${NAMESPACE} \
+    --volume $(pwd)/configuration:/etc/opt/${NAMESPACE} \
     --volume $(pwd):/var/opt/${NAMESPACE}/log \
     --volume $(pwd):/var/opt/${NAMESPACE}/data \
     dnguyen0304/${NAMESPACE}-runtime:${TAG}
