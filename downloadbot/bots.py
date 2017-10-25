694f50ac4f1b0dbc7d6de1a19c0848fe67507e03
ubuntu <dnguyen0304@gmail.com>
2017-10-25 13:30:06 -0400

    Changed PreValidating Bot into PostValidating Initializer.
    Changed Page Initializer to raise AutomationFailed.

--- downloadbot/bots.py
+++ downloadbot/bots.py
@@ -71,6 +71,7 @@ class Download(Disposable):
             If the battle has not yet completed.
         """
 
+        # TODO(duy): This raises an unhandled AutomationFailed exception.
         self._initializer.initialize(web_driver=self._web_driver, url=url)
         result = self._finder.find(locator=self._LOCATOR)
         try:
@@ -134,57 +135,6 @@ class Orchestrating(Disposable):
                             self._bot,
                             self._logger,
                             self._policy)
-
-
-class PreValidating(Disposable):
-
-    def __init__(self, bot, validator):
-
-        """
-        Extend to include validation.
-
-        Validation occurs before the bot runs.
-
-        Parameters
-        ----------
-        bot : downloadbot.bots.Disposable
-        validator : downloadbot.common.automation.validators.PokemonShowdown
-        """
-
-        self._bot = bot
-        self._validator = validator
-
-    def run(self, url):
-
-        """
-        Raises
-        ------
-        downloadbot.common.automation.exceptions.ConnectionLost
-            If the connection was not established successfully or was lost.
-        downloadbot.common.automation.exceptions.RoomExpired
-            If the room has expired.
-        """
-
-        try:
-            self._validator.check_room_was_entered()
-        except automation.exceptions.ValidationFailed:
-            try:
-                self._validator.check_connection_exists()
-            except automation.exceptions.ConnectionLost:
-                raise
-            else:
-                message = 'The room has expired.'
-                raise exceptions.RoomExpired(message)
-        self._bot.run(url=url)
-
-    def dispose(self):
-        self._bot.dispose()
-
-    def __repr__(self):
-        repr_ = '{}(bot={}, validator={})'
-        return repr_.format(self.__class__.__name__,
-                            self._bot,
-                            self._validator)
 
 
 class PostValidating(Disposable):
--- downloadbot/initializers.py
+++ downloadbot/initializers.py
@@ -2,6 +2,10 @@
 
 import abc
 
+from downloadbot.common import automation
+
+from . import exceptions
+
 
 # This could be evaluated for being migrated to common.
 class Page(metaclass=abc.ABCMeta):
@@ -20,6 +24,11 @@ class Page(metaclass=abc.ABCMeta):
         Returns
         -------
         None
+
+        Raises
+        ------
+        downloadbot.common.automation.exceptions.AutomationFailed
+            If the page was not initialized successfully.
         """
 
         raise NotImplementedError
@@ -33,3 +42,52 @@ class SeleniumPage(Page):
     def __repr__(self):
         repr_ = '{}()'
         return repr_.format(self.__class__.__name__)
+
+
+class PostValidating(Page):
+
+    def __init__(self, initializer, validator):
+
+        """
+        Extend to include validation.
+
+        Validation occurs after the page is initialized.
+
+        Parameters
+        ----------
+        initializer : downloadbot.initializers.Page
+        validator : downloadbot.common.automation.validators.PokemonShowdown
+        """
+
+        self._initializer = initializer
+        self._validator = validator
+
+    def initialize(self, web_driver, url):
+
+        """
+        Raises
+        ------
+        downloadbot.common.automation.exceptions.ConnectionLost
+            If the connection was not established successfully or was lost.
+        downloadbot.common.automation.exceptions.RoomExpired
+            If the room has expired.
+        """
+
+        self._initializer.initialize(web_driver=web_driver, url=url)
+
+        try:
+            self._validator.check_room_was_entered()
+        except automation.exceptions.ValidationFailed:
+            try:
+                self._validator.check_connection_exists()
+            except automation.exceptions.ConnectionLost:
+                raise
+            else:
+                message = 'The room has expired.'
+                raise exceptions.RoomExpired(message)
+
+    def __repr__(self):
+        repr_ = '{}(initializer={}, validator={})'
+        return repr_.format(self.__class__.__name__,
+                            self._initializer,
+                            self._validator)
