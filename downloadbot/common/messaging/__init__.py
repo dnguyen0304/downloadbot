5cd4ee89a2328611534ccf5033ef785ae71cb750
ubuntu <dnguyen0304@gmail.com>
2017-10-26 15:07:34 -0400

    Added consuming and events to package index.

--- downloadbot/common/messaging/__init__.py
+++ downloadbot/common/messaging/__init__.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from . import consuming
+from . import events
 from . import exceptions
 from . import filters
 from . import messages
 
-__all__ = ['exceptions',
+__all__ = ['consuming',
+           'events',
+           'exceptions',
            'filters',
            'messages']
