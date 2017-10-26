90a146b9f2735faff02f2d7649a651647b16072e
ubuntu <dnguyen0304@gmail.com>
2017-10-25 20:12:04 -0400

    Removed Printing bot.

--- .tmp/clare/clare/application/download_bot/download_bots.py
+++ .tmp/clare/clare/application/download_bot/download_bots.py
@@ -82,28 +82,3 @@ class LoggingDownloadBot(object):
         return repr_.format(self.__class__.__name__,
                             self._download_bot,
                             self._logger)
-
-
-class PrintingDownloadBot(object):
-
-    def __init__(self, download_bot):
-
-        """
-        Parameters
-        ----------
-        download_bot : clare.application.download_bot.download_bots.DownloadBot
-        """
-
-        self._download_bot = download_bot
-
-    def run(self, url):
-        file_path = self._download_bot.run(url=url)
-        print(file_path)
-        return file_path
-
-    def dispose(self):
-        self._download_bot.dispose()
-
-    def __repr__(self):
-        repr_ = '{}(download_bot={})'
-        return repr_.format(self.__class__.__name__, self._download_bot)
