1a23a664fdb694d32e80aee883dbc8a1bb20a4f0
ubuntu <dnguyen0304@gmail.com>
2017-10-25 21:25:01 -0400

    Removed migrated events.

--- .tmp/roomlistwatcher/common/messaging/__init__.py
+++ .tmp/roomlistwatcher/common/messaging/__init__.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from . import events
 from . import exceptions
 from . import filters
 from . import producing
 
-__all__ = ['events',
-           'exceptions',
+__all__ = ['exceptions',
            'filters',
            'producer']
