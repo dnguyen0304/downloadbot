f772ae8a8e73bb6ce4d99cff655c91b7737ea577
ubuntu <dnguyen0304@gmail.com>
2017-10-27 12:26:32 -0400

    Removed migrated messaging package.

--- .tmp/roomlistwatcher/common/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import io
-from . import messaging
-from . import utility
-
-__all__ = ['io',
-           'messaging',
-           'utility']
--- .tmp/roomlistwatcher/common/messaging/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import exceptions
-from . import filters
-from . import producing
-
-__all__ = ['exceptions',
-           'filters',
-           'producer']
