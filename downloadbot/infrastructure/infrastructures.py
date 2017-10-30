f531fdc42d159101f3558d2d87e1483217011855
ubuntu <dnguyen0304@gmail.com>
2017-10-30 14:49:50 -0400

    Changed DownloadBot infrastructure to require deleter.

--- downloadbot/infrastructure/infrastructures.py
+++ downloadbot/infrastructure/infrastructures.py
@@ -3,16 +3,20 @@
 
 class DownloadBot:
 
-    def __init__(self, receiver):
+    def __init__(self, receiver, deleter):
 
         """
         Parameters
         ----------
         receiver : downloadbot.common.messaging.consuming.receivers.Receiver
+        deleter : downloadbot.common.messaging.consuming.deleters.Deleter
         """
 
         self.receiver = receiver
+        self.deleter = deleter
 
     def __repr__(self):
-        repr_ = '{}(receiver={})'
-        return repr_.format(self.__class__.__name__, self.receiver)
+        repr_ = '{}(receiver={}, deleter={})'
+        return repr_.format(self.__class__.__name__,
+                            self.receiver,
+                            self.deleter)
