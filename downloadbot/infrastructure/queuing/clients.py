0254217f6ee3fc723c4344b3f9d76d16c81cdce2
ubuntu <dnguyen0304@gmail.com>
2017-11-04 10:02:41 -0400

    Added SqsFifo client.

--- downloadbot/infrastructure/queuing/clients.py
+++ downloadbot/infrastructure/queuing/clients.py
@@ -28,3 +28,32 @@ class Client(metaclass=abc.ABCMeta):
         """
 
         raise NotImplementedError
+
+
+class SqsFifo(Client):
+
+    def __init__(self, sqs_queue):
+
+        """
+        Parameters
+        ----------
+        sqs_queue : boto3.resources.factory.sqs.Queue
+        """
+
+        self._sqs_queue = sqs_queue
+
+    def delete_message(self, message):
+        request = {
+            'Entries': [
+                {
+                    'Id': message.id,
+                    'ReceiptHandle': message.delivery_receipt
+                }
+            ]
+        }
+        response = self._sqs_queue.delete_messages(**request)
+        return response
+
+    def __repr__(self):
+        repr_ = '{}(sqs_queue={})'
+        return repr_.format(self.__class__.__name__, self._sqs_queue)
