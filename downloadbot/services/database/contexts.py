a99f6c8943899c3b1d72e8a5c32de938993cd63c
Duy Nguyen <dnguyen0304@gmail.com>
2017-11-09 17:31:24 -0500

    ME-729: Added SqlAlchemy context.

--- downloadbot/services/database/contexts.py
+++ downloadbot/services/database/contexts.py
@@ -42,3 +42,32 @@ class Context(metaclass=abc.ABCMeta):
         """
 
         raise NotImplementedError
+
+
+class SqlAlchemy(Context):
+
+    def __init__(self, session):
+
+        """
+        Implementation backed by SQLAlchemy.
+
+        Parameters
+        ----------
+        session : sqlalchemy.orm.session.Session
+        """
+
+        self._session = session
+
+    def add(self, model):
+        # This should catch sqlalchemy.orm.exc.UnmappedInstanceError
+        # exceptions.
+        self._session.add(model)
+
+    def commit(self):
+        # This should catch sqlalchemy.exc.InvalidRequestError
+        # exceptions.
+        self._session.commit()
+
+    def __repr__(self):
+        repr_ = '{}(session={})'
+        return repr_.format(self.__class__.__name__, self._session)
