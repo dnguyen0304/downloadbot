db4689e4159a908a8ef83a07f4d95e23cd8e2c89
Duy Nguyen <dnguyen0304@gmail.com>
2017-11-09 16:31:55 -0500

    ME-729: Added Persistence handler.

--- downloadbot/services/database/handlers.py
+++ downloadbot/services/database/handlers.py
@@ -2,6 +2,8 @@
 
 import abc
 
+from . import models
+
 
 class Event(metaclass=abc.ABCMeta):
 
@@ -25,3 +27,38 @@ class Event(metaclass=abc.ABCMeta):
         """
 
         raise NotImplementedError
+
+
+class Persistence(Event):
+
+    def __init__(self, event_parser, repository):
+
+        """
+        Handler that writes to persistent storage.
+
+        Parameters
+        ----------
+        event_parser : downloadbot.services.database.parsers.S3ObjectCreatedEvent
+        repository : downloadbot.services.database.repositories.Repository
+        """
+
+        self._event_parser = event_parser
+        self._repository = repository
+
+    def handle(self, event):
+        try:
+            kwargs = self._event_parser.parse(event)
+        except ValueError:
+            return
+        try:
+            # This should use a model factory instead.
+            model = models.Replay(**kwargs)
+        except TypeError:
+            return
+        self._repository.add(model=model)
+
+    def __repr__(self):
+        repr_ = '{}(event_parser={}, repository={})'
+        return repr_.format(self.__class__.__name__,
+                            self._event_parser,
+                            self._repository)
