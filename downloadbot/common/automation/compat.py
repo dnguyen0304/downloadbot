e44d6e50b39930a7d6df963ed3c70a96bfecf69c
Duy Nguyen <dnguyen0304@gmail.com>
2017-11-10 11:26:01 -0500

    ME-729: Fixed compat failing on Python 3.5.x.

--- downloadbot/common/automation/compat.py
+++ downloadbot/common/automation/compat.py
@@ -5,12 +5,12 @@ import sys
 _MAJOR_AND_MINOR = slice(0, 2)
 _PYTHON_VERSION = sys.version_info[_MAJOR_AND_MINOR]
 _PYTHON_VERSION_27 = (2, 7)
-_PYTHON_VERSION_36 = (3, 6)
+_PYTHON_VERSION_3 = (3, 0)
 
 if _PYTHON_VERSION == _PYTHON_VERSION_27:
     import BaseHTTPServer as http_serving
     import httplib as HttpStatus
-elif _PYTHON_VERSION == _PYTHON_VERSION_36:
+elif _PYTHON_VERSION >= _PYTHON_VERSION_3:
     import http.server as http_serving
     from http import HTTPStatus as HttpStatus
 else:
