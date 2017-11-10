dbcc59f1b2d9f3c7a63a2372644b6c53c9db8079
Duy Nguyen <dnguyen0304@gmail.com>
2017-11-10 10:51:27 -0500

    ME-729: Changed handler to include logging.

--- downloadbot/services/database/factories.py
+++ downloadbot/services/database/factories.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import logging
+import logging.config
+
 import sqlalchemy
 import sqlalchemy.engine.url
 import sqlalchemy.orm
@@ -7,6 +10,44 @@ import sqlalchemy.orm
 from . import contexts
 from . import handlers
 from . import parsers
+
+
+class Logger:
+
+    def __init__(self, properties):
+
+        """
+        Parameters
+        ----------
+        properties : typing.Mapping
+        """
+
+        self._properties = properties
+
+    def create(self):
+
+        """
+        Returns
+        -------
+        logging.Logger
+
+        Raises
+        ------
+        KeyError
+            If an environment or property variable could not be found.
+        """
+
+        # Set the global configuration options.
+        logging.config.dictConfig(config=self._properties['logging'])
+
+        # Create the logger.
+        logger = logging.getLogger(name=self._properties['logger']['name'])
+
+        return logger
+
+    def __repr__(self):
+        repr_ = '<{}(properties={})>'
+        return repr_.format(self.__class__.__name__, self._properties)
 
 
 class EventHandler:
@@ -30,8 +71,13 @@ class EventHandler:
 
         Raises
         ------
-        None
+        KeyError
+            If an environment or property variable could not be found.
         """
+
+        # Create the logger.
+        logger_factory = Logger(properties=self._properties)
+        logger = logger_factory.create()
 
         # Create the event parser.
         event_parser = parsers.S3ObjectCreatedEvent()
@@ -64,6 +110,9 @@ class EventHandler:
         # Create the event handler.
         event_handler = handlers.Persistence(event_parser=event_parser,
                                              db_context=db_context)
+        # Include logging.
+        event_handler = handlers.Logging(event_handler=event_handler,
+                                         logger=logger)
         return event_handler
 
     def __repr__(self):
